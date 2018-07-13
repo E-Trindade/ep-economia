1fd6956520b4bc65d2215fd4040d73c0744b73b5
Eder Trindade <eder.gabriel.felix@gmail.com>
2018-07-13 04:22:13 -0300

    Equacoes de elasticidade

---
+++ app.js
@@ -0,0 +1,21 @@
+var app = angular.module('app', [])
+
+app
+    .controller('el_dem_ctrl', function($scope) {
+        $scope.qi = 0
+        $scope.qf = 1
+        $scope.pi = 0
+        $scope.pf = 1
+    })
+    .controller('el_ren_ctrl', function($scope) {
+        $scope.qi = 0
+        $scope.qf = 1
+        $scope.li = 0
+        $scope.lf = 1
+    })
+    .controller('el_cruz_ctrl', function($scope) {
+        $scope.qi = 0
+        $scope.qf = 1
+        $scope.li = 0
+        $scope.lf = 1
+    })
